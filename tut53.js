c9a589383fa6e3fe74bee66c3931fe81f3f00897
Apoorva01011 <138206931+Apoorva01011@users.noreply.github.com>
2023-08-01 00:24:36 +0530

    Add files via upload

---
+++ tut53.js
@@ -0,0 +1,12 @@
+console.log("jai mata di");
+function greet(n5,greetext="goooooodddd")
+{ console.log(n5+ "good");
+console.log(greetext);}
+n="harry";
+n1="su";
+n3="gg";
+greet(n);
+greet(n1,greetext);
+let greetext="blaaaa"
+// console.log(n1+"good");
+// console.log(n3+"good");
